6903242a96c44b8781f6119b7f2f557cf261dbb4
Erwin <winn92091229@gmail.com>
2019-04-10 22:16:09 -0400

    implementando cache with network fallback

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const STATIC_CACHE = "static-v1";
+// imports
+importScripts("js/sw-utils.js");
+const STATIC_CACHE = "static-v2";
 const DYNAMIC_CACHE = "dynamic-v1";
 const INMUTABLE_CACHE = "inmutable-v1";
 
@@ -14,7 +16,8 @@ const APP_SHELL = [
   "img/avatars/spiderman.jpg",
   "img/avatars/thor.jpg",
   "img/avatars/wolverine.jpg",
-  "js/app.js"
+  "js/app.js",
+  "js/sw-utils.js"
 ];
 
 const APP_SHELL_INMUTABLE = [
@@ -43,4 +46,17 @@ self.addEventListener("activate", e => {
     });
   });
   e.waitUntil(respuesta);
+});
+
+self.addEventListener("fetch", e => {
+  const respuesta = caches.match(e.request).then(res => {
+    if (res) {
+      return res;
+    } else {
+      return fetch(e.request).then(newRes => {
+        return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
+      });
+    }
+  });
+  e.respondWith(respuesta);
 });
