3b1aca5e676809d2c37bea79e88eac7f67831a61
Erwin <winn92091229@gmail.com>
2019-04-11 16:05:05 -0400

    modificaciones para ios

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Un cascarón de chat usando jQuery para PWAs
 
 # Run server
 `http-server`
+
+# Push to remotes repositories
+`git push && git push gitlab master`
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // imports
 importScripts("js/sw-utils.js");
-const STATIC_CACHE = "static-v3";
+const STATIC_CACHE = "static-v4";
 const DYNAMIC_CACHE = "dynamic-v1";
 const INMUTABLE_CACHE = "inmutable-v1";
 
